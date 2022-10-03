6573c8ca2fd16b571da1fb8c72680b40e606e183
Danny <dannypeterson28@gmail.com>
2022-10-03 12:03:12 -0400

    added buttons

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-let sayHello = () => {
-  console.log('Hello')
+//Hit Me and stay button functions
+const hitButton = document.querySelector('.hitButton')
+const stayButton = document.querySelector('.stayButton')
+
+const hitResponse = () => {
+  console.log('You drew another card!')
 }
 
-sayHello()
+const stayResponse = () => {
+  console.log('You decided to stay')
+}
+hitButton.addEventListener('click', hitResponse)
+stayButton.addEventListener('click', stayResponse)
